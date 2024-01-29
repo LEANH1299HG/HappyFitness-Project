1727403b2c493650573559ec667bab8434b44172
LEANH1299HG <anhlthe130946@fpt.edu.vn>
2024-01-29 17:04:20 +0700

    create new project

--- happy-fitness-angular/src/app/app.component.spec.ts
+++ happy-fitness-angular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-angular-app' title`, () => {
+  it(`should have the 'happy-fitness-anuglar' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-angular-app');
+    expect(app.title).toEqual('happy-fitness-anuglar');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-angular-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, happy-fitness-anuglar');
   });
 });
